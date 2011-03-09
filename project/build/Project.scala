e40d0f73035d0c6923354fdcfa1a9516748f59fa
Mads Hartmann <mads379@gmail.com>
2011-03-09 17:44:11 +0100

    Bumped version to 1.6 (depending on engine 0.6)

--- project/build/Project.scala
+++ project/build/Project.scala
@@ -2,11 +2,7 @@ import sbt._
 
 class Project(info: ProjectInfo) extends ProcessorProject(info) with AssemblyProject {
 
-  val scalatools_release = "Scala Tools Releases" at "http://scala-tools.org/repo-releases/"
-  val scalatools_snapshots = "Scala Tools Snapshot" at "http://scala-tools.org/repo-snapshots/"
-
-  val sbt_template_engine = "org.lifty" %% "lifty-engine" % "0.5"
-  val scalatest = "org.scalatest" % "scalatest" % "1.0" % "test"
+  val sbt_template_engine = "org.lifty" %% "lifty-engine" % "0.6"
 
   override def managedStyle = ManagedStyle.Maven
   val publishTo = "Scala Tools Nexus" at "http://nexus.scala-tools.org/content/repositories/releases/"
